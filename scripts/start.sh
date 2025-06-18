445ae4f14fc889541c4fffe92c2678732635bb6a
Roma Oganesyan <Roma.Oganesyan@everys.com>
2025-06-18 18:19:02 +0400

    Docker fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,9 @@ while ! pg_isready -h db -p 5432 -U landly-user; do
     sleep 2
 done
 
+export HOST=${HOST:-"0.0.0.0"}
+export PORT=${PORT:-"8080"}
+
 echo "Running database migrations..."
 diesel migration run
 
